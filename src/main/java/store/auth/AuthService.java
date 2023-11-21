c55b62b87d2ef9720e7b8b636713b5429afbdd4c
Humberto Sandmann <hsandmann@ieee.org>
2023-11-21 12:54:42 -0300

    login

--- src/main/java/store/auth/AuthService.java
+++ src/main/java/store/auth/AuthService.java
@@ -1,17 +1,32 @@
 package store.auth;
 
+import java.util.Date;
+
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import store.customer.CustomerController;
 import store.customer.CustomerOut;
 import store.customer.CustomertIn;
 
 @Service
 public class AuthService {
+
+    @Value("${store.jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${store.jwt.issuer}")
+    private String issuer;
+
+    @Value("${store.jwt.duration}")
+    private long duration;
+
 
     @Autowired
     private CustomerController customerController;
@@ -22,14 +37,17 @@ public class AuthService {
         return createJWT(found.id());
     }
 
-    private String createJWT(String id) {
-        SecretKey key = Jwts.SIG.HS256.key().build();
+    private String createJWT(String id) {        
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
         String jwt = Jwts.builder()
             .header()
             .keyId("EsPm.Store")
             .and()
+            .issuer(issuer)
             .subject(id)
             .signWith(key)
+            .notBefore(new Date())
+            .expiration(new Date(new Date().getTime() + duration))
             .compact();
         return jwt;
     }
